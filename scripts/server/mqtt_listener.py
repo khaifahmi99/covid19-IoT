cbaf5a52a343bb82619956a803a2efbd07add992
khaifahmi99 <khaifahmi99@github.com>
2020-10-19 10:31:02 +1100

    bug fix, tested

--- scripts/server/mqtt_listener.py
+++ scripts/server/mqtt_listener.py
@@ -5,21 +5,21 @@ Description: Main File to always listen as an MQTT broker. Currently, this is ju
 
 import paho.mqtt.client as mqtt
 
-MQTT_SERVER = "192.168.0.10"
+MQTT_SERVER = "localhost"
 PORT = 1883
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
     # set topic that wants to be listened
-    # client.subscribe('parking/status')
+    # client.subscribe('covid/test')
 
 def on_publish(client, userdata, result):
     print("data published")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    msg.payload = json.loads(msg.payload)
+    # msg.payload = json.loads(msg.payload) # might cause trouble
 
     print('Message Topic: ', msg.topic)
     print('Payload: ', msg.payload)
