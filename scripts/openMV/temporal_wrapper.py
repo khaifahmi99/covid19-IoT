eaab3038ffc8285baceead85f706fad84e68ad36
khaifahmi99 <khaifahmi99@github.com>
2020-10-29 17:48:23 +1100

    add temporal method based on window check (label)

--- scripts/openMV/temporal_wrapper.py
+++ scripts/openMV/temporal_wrapper.py
@@ -7,7 +7,7 @@ import sensor, image, time, os, tf, pyb
 
 class TemporalWrapper():
 
-    def __init__(self, net, labels, window_size, name="Topic"):
+    def __init__(self, net, labels, window_size, positive_class, confidence_thres=0.7, name="Topic"):
         self.net = net
         self.labels = labels
         self.window_size = window_size
@@ -16,6 +16,10 @@ class TemporalWrapper():
         self.blue_led = pyb.LED(3)
         self.clock = time.clock()
         self.name = name
+        self.preds = []
+        self.scores = []
+        self.positive_class = positive_class
+        self.confidence_thres = confidence_thres
 
     def predict(self, img, name):
         for obj in tf.classify(self.net, img, min_scale=1.0, scale_mul=0.8, x_overlap=0.5, y_overlap=0.5):
@@ -25,12 +29,43 @@ class TemporalWrapper():
             # This combines the labels and confidence values into a list of tuples
             predictions_list = list(zip(self.labels, obj.output()))
 
+            # print results on screen and terminal
             for i in range(len(predictions_list)):
                 print("%s = %f" % (predictions_list[i][0], predictions_list[i][1]))
 
                 # draw a rectangle and the predicted label on screen
                 img.draw_rectangle(obj.rect())
                 img.draw_string(obj.x()+3, obj.y()-1, self.labels[obj.output().index(max(obj.output()))], mono_space = False)
+
+            results = self.format_predictions(predictions_list)
+            if len(self.preds) >= self.window_size:
+                self.preds = self.preds[1:]
+
+            if results['class'] == self.positive_class:
+                self.preds.append(results['class'])
+            else:
+                self.preds.append(None)
+
+            if None in self.preds:
+                self.green_led.on()
+                self.red_led.off()
+            else:
+                self.red_led.on()
+                self.green_led.off()
+
+    def format_predictions(self, arr):
+        c = 0
+        compared_confidence = 0
+        for i in range(len(arr)):
+            if arr[i][1] > compared_confidence:
+                compared_confidence = arr[i][1]
+                c = i
+
+        results = {}
+        results['class'] = arr[c][0]
+        results['confidence'] = compared_confidence
+        return results
+
 
     def run(self):
         while(True):
@@ -51,5 +86,5 @@ sensor.skip_frames(time=2000)          # Let the camera adjust.
 net = "trained.tflite"
 labels = [line.rstrip('\n') for line in open("labels.txt")]
 
-runner = TemporalWrapper(net, labels, 4)
+runner = TemporalWrapper(net, labels, 20, "cough")
 runner.run()
